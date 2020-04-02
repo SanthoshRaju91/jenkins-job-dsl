aeb486410c1db18f21e3a0c5079ad6a6d3475066
santhoshRaju91 <santhosh.codeholic@gmail.com>
2020-04-02 23:01:45 +0530

    First jenkins DSL groovy

---
+++ nodejs.groovy
@@ -0,0 +1,17 @@
+job('nodejs-job-dsl') {
+    scm {
+        git('git://github.com/wardviaene/docker-demo.git') { node ->
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@do.com')
+        }
+    }
+    trigger {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        shell(npm install)
+    }
+}
